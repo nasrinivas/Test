9150ad3eb6ed1d7276cf12468e1423a388506697
nasrinivas <104984820+nasrinivas@users.noreply.github.com>
2024-03-27 12:46:56 +0530

    Update var.tf
    2

--- var.tf
+++ var.tf
@@ -1,103 +1,30 @@
-# Define variables
-variable "vpc_cidr_block" {
+Terraform
+variable "vpc_cidr" {
+  type = string
   description = "CIDR block for the VPC"
 }
 
-variable "subnet_cidr_blocks" {
-  description = "CIDR blocks for subnets in each availability zone"
-  type        = list(string)
+variable "availability_zones" {
+  type = list(string)
+  description = "List of availability zones for subnets"
 }
 
-variable "acl_rules" {
-  description = "ACL rules provided by the user"
-  type        = list(object({
-    rule_number   = number
-    action        = string
-    cidr_block    = string
-    protocol      = number
-    from_port     = number
-    to_port       = number
-  }))
+variable "instance_type" {
+  type = string
+  description = "EC2 instance type (e.g., t2.micro)"
 }
 
-variable "security_group_rules" {
-  description = "Security group rules provided by the user"
-  type        = list(object({
-    type        = string
-    cidr_blocks = list(string)
-    from_port   = number
-    to_port     = number
-    protocol    = string
-  }))
+variable "ssh_key_name" {
+  type = string
+  description = "Name of existing SSH key pair"
 }
 
-# Create VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr_block
-  enable_dns_support = true
-  enable_dns_hostnames = true
+variable "user_data" {
+  type = string
+  description = "User data script for the EC2 instance (optional)"
 }
 
-# Create subnets in each availability zone
-resource "aws_subnet" "my_subnets" {
-  count             = length(var.subnet_cidr_blocks)
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.subnet_cidr_blocks[count.index]
-  availability_zone = "us-west-2a" # Replace with your desired AZ
-}
-
-# Create ACL rules
-resource "aws_network_acl" "my_acl" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = var.acl_rules[0].from_port
-    to_port     = var.acl_rules[0].to_port
-    protocol    = var.acl_rules[0].protocol
-    rule_action = var.acl_rules[0].action
-    cidr_block  = var.acl_rules[0].cidr_block
-  }
-
-  egress {
-    from_port   = var.acl_rules[1].from_port
-    to_port     = var.acl_rules[1].to_port
-    protocol    = var.acl_rules[1].protocol
-    rule_action = var.acl_rules[1].action
-    cidr_block  = var.acl_rules[1].cidr_block
-  }
-}
-
-# Create security group
-resource "aws_security_group" "my_security_group" {
-  name        = "my_security_group"
-  description = "My Security Group"
-
-  ingress {
-    from_port   = var.security_group_rules[0].from_port
-    to_port     = var.security_group_rules[0].to_port
-    protocol    = var.security_group_rules[0].protocol
-    cidr_blocks = var.security_group_rules[0].cidr_blocks
-  }
-
-  egress {
-    from_port   = var.security_group_rules[1].from_port
-    to_port     = var.security_group_rules[1].to_port
-    protocol    = var.security_group_rules[1].protocol
-    cidr_blocks = var.security_group_rules[1].cidr_blocks
-  }
-
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-# Create EC2 instance
-resource "aws_instance" "my_instance" {
-  ami           = "ami-12345678" # Replace with your desired AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_subnets[0].id # Use the first subnet created
-
-  security_groups = [aws_security_group.my_security_group.name]
-
-  tags = {
-    Name = "MyEC2Instance"
-  }
+variable "tags" {
+  type = map(string)
+  description = "Tags for the EC2 instance"
 }
